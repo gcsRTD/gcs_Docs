fbe2ee90b6c886b81b7f1b758c9bfa07f31a9cf8
gcsRTD <36299350+gcsRTD@users.noreply.github.com>
2018-02-14 16:48:33 +0000

    Add files via upload

--- _sources/conf.py
+++ _sources/conf.py
@@ -31,9 +31,9 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-#extensions = []
-extensions = ['rst2pdf.pdfbuilder']
-pdf_documents =[('index', u'documentName', u'Nome do Documento', u'GCS Document'),]
+extensions = []
+#extensions = ['rst2pdf.pdfbuilder']
+#pdf_documents =[('index', u'documentName', u'Nome do Documento', u'GCS Document'),]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
